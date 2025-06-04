4fe090c22f5eddc380e57a47242eeabee136b9ae
elpajaro <sstepin425@gmail.com>
2025-06-04 23:48:57 +0300

    tests

--- bash/tests.sh
+++ bash/tests.sh
@@ -1,6 +1,14 @@
-docker-compose down -v
 docker-compose build
+
+# kafka tests
+docker-compose down -v
 docker-compose up -d
 sleep 10
 docker-compose down statistics
 docker-compose up tests_kafka_producer
+
+# e2e tests
+docker-compose down -v
+docker-compose up -d
+sleep 10
+docker-compose up tests_e2e
